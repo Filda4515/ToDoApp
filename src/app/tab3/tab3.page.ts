3d547c1e6638d98cff726e0dbf9784d9dbeca56c
Filda4515 <48486481+Filda4515@users.noreply.github.com>
2025-01-19 14:30:07 +0100

    Dark mode

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,7 +7,28 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab3Page {
+  paletteToggle = false;
 
   constructor() {}
 
+  ngOnInit() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    this.initializeDarkPalette(prefersDark.matches);
+
+    prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkPalette(mediaQuery.matches));
+  }
+
+  initializeDarkPalette(isDark: boolean) {
+    this.paletteToggle = isDark;
+    this.toggleDarkPalette(isDark);
+  }
+
+  toggleChange(event: CustomEvent) {
+    this.toggleDarkPalette(event.detail.checked);
+  }
+
+  toggleDarkPalette(shouldAdd: boolean) {
+    document.documentElement.classList.toggle('ion-palette-dark', shouldAdd);
+  }
 }
