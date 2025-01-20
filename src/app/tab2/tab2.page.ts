e5e73a5158cc8d5732a5eec8f1aea2d11e3445b3
Filda4515 <48486481+Filda4515@users.noreply.github.com>
2025-01-20 07:53:05 +0100

    native calendar (no permissions)

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,6 +5,9 @@ import { AppStorageService } from '../app-storage.service';
 import { ITEMS_STORAGE } from '../app.constants';
 import { IonDatetime } from '@ionic/angular';
 
+import { AlertController } from '@ionic/angular';
+import { CapacitorCalendar } from '@ebarooni/capacitor-calendar';
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -16,7 +19,7 @@ export class Tab2Page {
   items: ToDoItem[] = [];
   newItem: ToDoItem = new ToDoItem('', false);
 
-  constructor(private appStorage: AppStorageService) {}
+  constructor(private appStorage: AppStorageService, private alertController: AlertController) {}
 
   async ionViewDidEnter()  {
     const loaded_items = await this.appStorage.get(ITEMS_STORAGE);
@@ -30,6 +33,46 @@ export class Tab2Page {
     this.items.push(this.newItem);
     this.appStorage.set(ITEMS_STORAGE, this.items);
     this.resetForm();
+
+    const confirmation = await this.alertController.create({
+      header: 'Add to Calendar?',
+      message: 'Do you want to add this task to your device calendar?',
+      buttons: [
+        {
+          text: 'No'
+        },
+        {
+          text: 'Yes',
+          handler: () => this.addToNativeCalendar()
+        },
+      ],
+    });
+
+    await confirmation.present();
+  }
+
+  async addToNativeCalendar() {
+    try {
+      await CapacitorCalendar.createEvent({
+        title: this.newItem.text,
+        endDate: this.newItem.date?.getDate()
+      });
+
+      const alert = await this.alertController.create({
+        header: 'Success',
+        message: 'Task added to your calendar.',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    } catch (error) {
+      console.error('Error adding event to calendar:', error);
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Failed to add task to the calendar.',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    }
   }
 
   resetForm() {
